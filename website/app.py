f3b692412836233d7783025ce8b136b29e140d17
Morgan Jensen <morganjensen@unr.edu>
2024-12-06 12:07:38 -0800

    Survey page implamentation

--- website/app.py
+++ website/app.py
@@ -4,6 +4,7 @@ from sessionManager import sessionManager
 import secrets
 from db_connection import create_tables
 from admin import admin
+from survey import Survey
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -18,6 +19,7 @@ def index():
 app.add_url_rule('/login', view_func=sessionManager.as_view('login', action='login'), methods=['GET', 'POST'])
 app.add_url_rule('/logout', view_func=sessionManager.as_view('logout', action='logout'), methods=['GET'])
 app.add_url_rule('/admin', view_func=admin.as_view('admin'), methods=['GET', 'POST'])
+app.add_url_rule('/survey', view_func=Survey.as_view('survey'), methods=['GET','POST'])
 
 # NOTE: Temporary until figuring out best way for game analytics pages
 @app.route('/chess')
@@ -35,10 +37,6 @@ def valorant():
 @app.route('/rocket_league')
 def rocket():
     return render_template('rocket.html')
-
-@app.route('/survey')
-def survey():
-    return render_template('survey.html')
 
 if __name__ == "__main__":
     # change debug to false to hide error from displaying on website, set to true for testing purposes
