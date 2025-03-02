b3ff81ffa24cf812af54d24423619815e0547295
carsonramo <carson.ramocinski2021@gmail.com>
2025-03-02 13:42:40 -0800

    added some admin page functionality

--- website/admin.py
+++ website/admin.py
@@ -13,36 +13,57 @@ class admin(View):
 
     def get_users(self):
         query = """
-        SELECT user_id FROM User_Information;
+        SELECT participant_id FROM Participants;
         """
         self.db_connection.execute_query(query)
-        all_users = [row[0] for row in self.db_connection.cursor.fetchall()]
+        all_users = [row['participant_id'] for row in self.db_connection.cursor.fetchall()]
         return all_users
+    
+    def upload_files(self, participant_id, files):
+        base_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+        data_dir = os.path.join(base_dir, 'session_data')
+        participant_dir = os.path.join(data_dir, f'participant_{participant_id}')
 
+        if not os.path.exists(data_dir):
+           os.makedirs(data_dir)
+
+        if not os.path.exists(participant_dir):
+            os.makedirs(participant_dir)  
+        
+        for file_key, file in files.items():
+            if file:
+                file_path = os.path.join(participant_dir, file.filename)
+                file.save(file_path)
+                if file_key == 'emotibit_file':
+                    unparsed_file = file_path
+                elif file_key == 'tracking_file':
+                    mktracking_file = file_path
+                elif file_key == 'timestamp_file':
+                    timestamp_file = file_path
+        # TODO: add timestamp file to function, re-work function for exact files from admin (split apart), need way to get session_id if needed
+        # DatabaseConnection.execute_addition(participant_id=participant_id, unparsed_file=unparsed_file, mktracking_file=mktracking_file)
     def dispatch_request(self):
         # NOTE: update to new database for admin privledge
-        if session.get('user_id') == 1:
+        # if session.get('user_id') == 1:
             if request.method == "POST":
-                print("upload")
-                print("Request Form Data:", request.form)
                 # dictionary used to get files
                 # TODO: Use dictionary to upload files 
                 user_id = request.form.get('user_id')
                 files = {
                     'emotibit_file': request.files.get('emotibit_file'),
-                    'hr_file': request.files.get('hr_file'),
                     'tracking_file': request.files.get('tracking_file'),
                     'timestamp_file': request.files.get('timestamp_file'),
-                    'dataframe_file': request.files.get('dataframe_file')
                 }
                 # TODO: call database class to upload file
                 # TODO: add {file.database_file_field}?
                 # TODO: Check if upload successful and change message based on success
-                print(f'Uploaded: User_id = {user_id}, file: {files}')
+                self.upload_files(user_id, files)
+                # print(files)
+                # print(f'Uploaded: User_id = {user_id}, file: {files}')
                 flash('Successfully upload files')
-                return redirect(url_for('index'))
+                # return redirect(url_for('index'))
             users = self.get_users()
             return render_template('admin.html', users=users)
-        else:
-            flash('Sorry, you need to be an admin to access that page.')
-            return redirect(url_for('index'))
+        # else:
+        #     flash('Sorry, you need to be an admin to access that page.')
+        #     return redirect(url_for('index'))
